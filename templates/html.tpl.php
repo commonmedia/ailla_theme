97ff59bef56f7f48cafcbcc015af39b02ac2b6a7
bjc2265 <bcornell@austin.utexas.edu>
2017-03-23 17:49:23 -0500

    Update html.tpl.php

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -11,5 +11,18 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+  <script>
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-3780897-25']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
